de0e00a82a394361d38a28ecabbaf68b6f16c212
Mathias Elmhauge Petersen <math955e@edu.easj.dk>
2020-03-08 12:45:29 +0100

    Commit

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,33 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+
+let doItButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("DoIt");
+doItButton.addEventListener("click", DoItButton)
+
+function DoItButton(): void {
+
+let inputField1: HTMLInputElement = <HTMLInputElement>document.getElementById("inputField1");
+let inputField1String: string = inputField1.value;
+
+let CaseOption: HTMLInputElement = <HTMLInputElement> document.getElementById("case");
+let CaseOptionValue: string = CaseOption.value.toString();
+
+let Result: string;
+
+if(CaseOptionValue == "UpperCase")
+{
+  
+  Result = inputField1String.toUpperCase();
+
+  let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+      element.innerHTML = Result;
+
+}
+else if(CaseOptionValue == "LowerCase"){
+  Result = inputField1String.toLowerCase();
+ 
+  let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+      element.innerHTML = Result;
+}
+    
+}
